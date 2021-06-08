a3c527ca20b96de9618f1a40ff0e496b25e06c7e
EAD\ggallo3 <ggallo3@dxc.com>
2021-06-08 14:20:56 -0300

    add module figuras

---
+++ math_module/figuras.py
@@ -0,0 +1,36 @@
+
+class Figura:
+    name = None
+
+    def __init__(self, name):
+        self.name = name
+
+    def description(self):
+        return {"name": self.name}
+
+
+class Poligono(Figura):
+    lados = []
+
+    def __init__(self,name, lados):
+        self.lados = lados
+        super().__init__(name)
+
+    def description(self):
+        return {"name": self.name, "lados": self.lados}
+
+    def perimetro(self):
+        return sum(self.lados)
+
+class Cuadrado(Poligono):
+    def __init__(self,name, lado):
+        super(Cuadrado, self).__init__(name, ([lado]*4))
+
+
+if __name__ == "__main__":
+    #figura = Figura("Cuadrado 1")
+    #figura = Poligono("Cuadrado 2", 2.3, 2.3, 2.3, 2.3)
+    figura = Cuadrado("Cuadrado 3", 2.3)
+    print(figura.description())
+    print(f'Perímetro {figura.perimetro()}')
+
