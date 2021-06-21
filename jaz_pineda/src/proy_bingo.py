9873f448a635fef71b6410d56810fc9f6a99737c
Jazmin <jppinedac940@hotmail.com>
2021-06-21 14:06:33 -0300

    actualizacion bingo casi finalizado xp

--- jaz_pineda/src/proy_bingo.py
+++ jaz_pineda/src/proy_bingo.py
@@ -1,49 +1,62 @@
 import random #genera nùmeros aleatoriamente
+class carton:
 
-
-def armar_carton(numeros):
-# numeros random para armar carton 
-# sample(sec, num). Esta función devuelve num elementos aleatorios de la secuencia sec.
-# acción de repartir num cartas a un jugador.
-    n_arma_cart1= random.sample(numeros, 20)
+    min= 1
+    max= 15
+    texto_b='BINGO'
+    card={} #crea cartilla
     
+    def __init__(self):
+        for letra in self.texto_b:
+            self.card[letra] = random.sample(range(self.min, self.max), 5)#toma una muestra del rango
+            self.min += 15
+            self.max += 15
     
-    celda= {'valor', 'numero', 'estado'} #creo dicc
-    carton = []
+    def imprimir_card(self):
+        for letra in self.card:
+            print(letra, end='\t')
+            for numero in self.card[letra]:
+                print(numero, end='\t')
+            print()
+        print()
 
+class bingo:  
+    def __init__(self):
+        self.carton = carton()
+        self.numer_bolilla = random.sample(range(1,76), 75)
 
-    for i in range(0, len(n_arma_cart1), 5): #  i = 0, 5
-        columna=[]
-        for num in n_arma_cart1[0 + i: 5 + i]: # 1 n_arma_cart1[0:5] 2 n_arma_cart1[5,10] 
-            columna.append({'valor': num, 'formato':f"{num:02}",'estado':False })
-        carton.append(columna)
-    return carton
-
-def imprimir(carton):
-    fieldnames = ['B', 'I', 'N', 'G', 'O']
-    for n in fieldnames:
-        print(n,  end='\t')
-    print()
-    for columna in carton:
-        for celda in columna:
-            print(celda['formato'], end='\t' )
-        print() #tiene salto de linea
-
-def main():
-    print("Bienvenido al juego Multi Bingo ")
-    #numeros q salen en la bolilla
-    numeros=[]
-    for i in range(1, 100):
-        numeros.append(i)
-
-    carton1 = armar_carton(numeros)
-    carton2 = armar_carton(numeros)
-    imprimir(carton1)
-    print()
-    # imprimir(carton2)
+    def sacar_bolilla(self):
+        numr_bolilla = self.numer_bolilla.pop()#sacar bolilla uno a uno
+        for letra in self.carton.card:
+            i = 0
+            for numero in self.carton.card[letra]:
+                if numero == numr_bolilla:
+                    self.carton.card[letra][i]='X'
+                i += 1
+        return numr_bolilla
     
+    #verifica carton si gana
+    def check_carton(self):
+        ganador= False
+        if self.carton.card['B'][0] =='X' and self.carton.card['I'][1]=='X' and self.carton.card['N'][2]=='X' and self.carton.card['G'][3]=='X' and self.carton.card['O'][4]=='X':
+            ganador= True
+        elif self.carton.card['O'][0] =='X' and self.carton.card['G'][1]=='X' and self.carton.card['N'][2]=='X' and self.carton.card['I'][3]=='X' and self.carton.card['B'][4]=='X':
+            ganador= True
+        for letra in self.carton.card:
+            if self.carton.card[letra].count('X') == len(self.carton.card[letra]):
+                ganador=True
+        return ganador
+
+if __name__=='__main__':
+    # carton = carton()
+    # carton.imprimir_card()
+    juego = bingo()
+    juego.carton.imprimir_card()
+    print("La bolilla es...", juego.sacar_bolilla())
+    respuesta=''
+    while respuesta != "Salir":
+        juego.carton.imprimir_card()
+        print("La bolilla es...", juego.sacar_bolilla())
+        respuesta= input()
 
 
-if __name__ == "__main__":
-    main()
-   
